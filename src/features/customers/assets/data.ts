b3f28cc83fea818dfd4161a430d13b1c8c860e2f
Alan Dsilva <alandsilva2001@gmai.com>
2022-09-05 16:52:37 +0530

    add files for backup

---
+++ src/features/customers/assets/data.ts
@@ -0,0 +1,10 @@
+import { ICustomer } from 'features/customers';
+
+export const data: ICustomer[] = [
+  {
+    firstName: 'Alan',
+    lastName: 'Dsilva',
+    email: 'alandsilva@gmail.com',
+    dob: '26/04/2001',
+  },
+];
---
+++ src/features/customers/types/index.ts
@@ -0,0 +1,24 @@
+export interface ICustomer {
+  firstName: string;
+  lastName: string;
+  email: string;
+  dob: string;
+}
+
+export class Customer implements ICustomer {
+  public firstName: string;
+  public lastName: string;
+  public email: string;
+  public dob: string;
+
+  constructor({ firstName = '', lastName = '', email, dob }: ICustomer) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.dob = dob;
+  }
+
+  getName(): string {
+    return this.firstName + this.lastName;
+  }
+}
